ca8837e60ed3c781b731323d0f8b7871f9f103d4
camila <Camilasantosgbarboza091@gmail.com>
2023-04-13 21:08:56 -0300

    exercicio js

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+alert('-----Média-----');
+let n1 = prompt('Nota 1:');
+let n2 = prompt('Nota 2:');
+let n3 = prompt('Nota 3:');
+let n4 = prompt('Nota 4:');
+alert (`Sua média foi: ${resultado}`); 
+resultado.tofixed(2) = n1 + n2 + n3 + n4 / 4;
+r = alert(`Resultado: ${resultado}`);
+
+
+if (resultado >= 6)
+{
+    alert('Aprovado');
+}
+else 
+{
+    alert('Reprovado');
+}
