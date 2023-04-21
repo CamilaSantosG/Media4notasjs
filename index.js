bfb3e1dd898951b903706e446bfec1da4801dd01
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-04-20 22:28:39 -0300

    exercicio

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 
 
 alert('-----Média-----');
-let n1 = prompt('Nota 1:');
-let n2 = prompt('Nota 2:');
-let n3 = prompt('Nota 3:');
-let n4 = prompt('Nota 4:');
-alert (`Sua média foi: ${resultado}`); 
-resultado.tofixed(2) = n1 + n2 + n3 + n4 / 4;
-r = alert(`Resultado: ${resultado}`);
+let n1 = parseFloat(prompt('Nota 1:'));
+let n2 = parseFloat(prompt('Nota 2:'));
+let n3 = parseFloat(prompt('Nota 3:'));
+let n4 = parseFloat(prompt('Nota 4:'));
+// function resultado(n1,n2,n3,n4){
+//     return 
+// }
+const resultado = (n1+n2+n3+n4)/4;
+ alert (`Sua média foi: ${resultado}`); 
+
 
 
 if (resultado >= 6)
 {
-    alert('Aprovado');
+    alert('Parabéns você foi aprovado🥳');
 }
 else 
 {
